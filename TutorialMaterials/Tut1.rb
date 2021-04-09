ec2076f7c350e39734b91bd550b0d0380e00bc4c
Ubuntu <ubuntu@ip-172-31-20-186.ec2.internal>
2021-04-09 02:53:25 +0000

    first commit

---
+++ TutorialMaterials/Tut1.rb
@@ -0,0 +1,22 @@
+#1a
+assignment = 10
+exam = 10
+
+sum = assignment +
+      exam
+
+puts sum
+
+#1b
+#Syntax not allowed (Need to put operator at the end of the statement)
+# tax = income – threshold 
+#       * tax_rate
+
+#1c
+#Syntax also incorrect 
+# a = 4
+# b = 6
+# average = a + b \
+# 2
+
+# puts average
